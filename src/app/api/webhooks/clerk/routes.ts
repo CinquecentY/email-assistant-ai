b4969097f86f4e8e8ca1559b6aaf12d3954dce4a
yannsqli <yannagrebah@sqli.com>
2024-11-27 11:56:34 +0100

    add clerk webhook

---
+++ src/app/api/webhooks/clerk/routes.ts
@@ -0,0 +1,15 @@
+import { db } from "@/server/db";
+
+export const POST = async (req: Request) => {
+    const { data } = await req.json();
+    const emailAddress = data.email_addresses[0].email_address;
+    const id = data.id;
+
+    await db.users.upsert({
+        where: { id },
+        update: {email: emailAddress},
+        create: { id,email: emailAddress},
+    });
+
+    return new Response('Webhook received', { status: 200 });
+}
