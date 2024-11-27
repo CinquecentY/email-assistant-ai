0448016f6ef938c836079b407ace07c377f0af0c
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-11-27 12:46:00 +0100

    test webhook

--- src/app/api/webhooks/clerk/routes.ts
+++ src/app/api/webhooks/clerk/routes.ts
@@ -4,12 +4,13 @@ export const POST = async (req: Request) => {
     const { data } = await req.json();
     const emailAddress = data.email_addresses[0].email_address;
     const id = data.id;
+console.log(emailAddress,id);
 
-    await db.users.upsert({
+    /*await db.users.upsert({
         where: { id },
         update: {email: emailAddress},
         create: { id,email: emailAddress},
-    });
+    });*/
 
     return new Response('Webhook received', { status: 200 });
 }
