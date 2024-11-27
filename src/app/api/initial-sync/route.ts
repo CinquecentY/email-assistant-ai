f00563eaed024a8764232e61b7b185b362b29f21
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-11-27 15:30:18 +0100

    add initial sync callback for emails syncing

---
+++ src/app/api/initial-sync/route.ts
@@ -0,0 +1,44 @@
+import Account from "@/lib/account";
+import { db } from "@/server/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export const maxDuration = 300;
+
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+  const { accountId, userId } = body;
+  if (!accountId || !userId)
+    return NextResponse.json({ error: "INVALID_REQUEST" }, { status: 400 });
+
+  const dbAccount = await db.accounts.findUnique({
+    where: {
+      id: accountId,
+      userId,
+    },
+  });
+  if (!dbAccount)
+    return NextResponse.json({ error: "ACCOUNT_NOT_FOUND" }, { status: 404 });
+
+  const account = new Account(dbAccount.token);
+  //await account.createSubscription();
+  const response = await account.performInitialSync();
+  if (!response)
+    return NextResponse.json({ error: "FAILED_TO_SYNC" }, { status: 500 });
+
+  const { deltaToken, emails } = response;
+
+  console.log(deltaToken, emails);
+
+  /*await syncEmailsToDatabase(emails, accountId)
+
+    await db.accounts.update({
+        where: {
+            token: dbAccount.token,
+        },
+        data: {
+            nextDeltaToken: deltaToken,
+        },
+    });*/
+  console.log("sync complete", deltaToken);
+  return NextResponse.json({ success: true, deltaToken }, { status: 200 });
+};
