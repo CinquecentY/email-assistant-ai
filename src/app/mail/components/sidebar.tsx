8e2f49a05e6bc71eaef04c5b01588f3d0613510c
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-11-27 20:05:43 +0100

    update sidebar.tsx component add count of each tab

--- src/app/mail/components/sidebar.tsx
+++ src/app/mail/components/sidebar.tsx
@@ -2,12 +2,38 @@ import React from "react";
 import { useLocalStorage } from "usehooks-ts";
 import { Nav } from "./nav";
 import { Inbox, Send, File } from "lucide-react";
+import { api } from "@/trpc/react";
 
 type SidebarProps = { isCollapsed: boolean };
 
 const Sidebar = ({ isCollapsed }: SidebarProps) => {
   const [tab] = useLocalStorage("email-assistant-ai-tab", "inbox");
   const [accountId] = useLocalStorage("accountId", "");
+
+  const refetchInterval = 5000;
+  const { data: inboxThreads } = api.mail.getNumThreads.useQuery(
+    {
+      accountId,
+      tab: "inbox",
+    },
+    { enabled: !!accountId && !!tab, refetchInterval },
+  );
+
+  const { data: draftsThreads } = api.mail.getNumThreads.useQuery(
+    {
+      accountId,
+      tab: "drafts",
+    },
+    { enabled: !!accountId && !!tab, refetchInterval },
+  );
+
+  const { data: sentThreads } = api.mail.getNumThreads.useQuery(
+    {
+      accountId,
+      tab: "sent",
+    },
+    { enabled: !!accountId && !!tab, refetchInterval },
+  );
 
   return (
     <>
@@ -16,19 +42,19 @@ const Sidebar = ({ isCollapsed }: SidebarProps) => {
         links={[
           {
             title: "Inbox",
-            label: "1",
+            label: inboxThreads?.toString() || "0",
             icon: Inbox,
             variant: tab === "inbox" ? "default" : "ghost",
           },
           {
             title: "Drafts",
-            label: "4",
+            label: draftsThreads?.toString() || "0",
             icon: File,
             variant: tab === "drafts" ? "default" : "ghost",
           },
           {
             title: "Sent",
-            label: "0",
+            label: sentThreads?.toString() || "0",
             icon: Send,
             variant: tab === "sent" ? "default" : "ghost",
           },
