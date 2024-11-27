419dc2ab631adf382e39f1316887da3deb9f73df
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-11-28 00:47:02 +0100

    update email-editor component add menu-bar.tsx add tag-input.tsx

--- src/app/mail/components/email-editor/index.tsx
+++ src/app/mail/components/email-editor/index.tsx
@@ -6,6 +6,12 @@ import { useLocalStorage } from "usehooks-ts";
 import { EditorContent, useEditor } from "@tiptap/react";
 import { StarterKit } from "@tiptap/starter-kit";
 import Text from "@tiptap/extension-text";
+import TipTapMenuBar from "./menu-bar";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import TagInput from "./tag-input";
+import { Input } from "@/components/ui/input";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 
 type EmailEditorProps = {
@@ -36,44 +42,108 @@ const EmailEditor = ({
   onCcChange,
   defaultToolbarExpand,
 }: EmailEditorProps) => {
-    //const [ref] = useAutoAnimate();
-    const [accountId] = useLocalStorage('accountId', '');
-    //const { data: suggestions } = api.mail.getEmailSuggestions.useQuery({ accountId: accountId, query: '' }, { enabled: !!accountId });
-    const [value, setValue] = React.useState('');
+  const [ref] = useAutoAnimate();
+  const [accountId] = useLocalStorage("accountId", "");
+  const { data: suggestions } = api.mail.getEmailSuggestions.useQuery({ accountId: accountId, query: '' }, { enabled: !!accountId });
+  const [value, setValue] = React.useState("");
 
+  const [expanded, setExpanded] = React.useState(defaultToolbarExpand ?? false);
 
-    const [expanded, setExpanded] = React.useState(defaultToolbarExpand ?? false);
+  const [generation, setGeneration] = React.useState("");
 
-    const [generation, setGeneration] = React.useState('');
-
-    const customText = Text.extend({
-        addKeyboardShortcuts() {
-            return {
-                "Meta-j": () => {
-                    //aiGenerate(this.editor.getText());
-                    return true;
-                },
-            };
+  const customText = Text.extend({
+    addKeyboardShortcuts() {
+      return {
+        "Meta-j": () => {
+          //aiGenerate(this.editor.getText());
+          return true;
         },
-    });
+      };
+    },
+  });
 
-    const editor = useEditor({
-        autofocus: false,
-        extensions: [StarterKit, customText/*, GhostExtension*/],
-        editorProps: {
-            attributes: {
-                placeholder: "Write your email here..."
-            }
-        },
-        onUpdate: ({ editor, transaction }) => {
-            setValue(editor.getHTML())
-        }
-    });
-    return(<div>
-        <div className="prose w-full px-4">
-                <EditorContent value={value} editor={editor} placeholder="Write your email here..." />
-            </div>
-    </div>)
+  const editor = useEditor({
+    autofocus: false,
+    extensions: [StarterKit, customText /*, GhostExtension*/],
+    editorProps: {
+      attributes: {
+        placeholder: "Write your email here...",
+      },
+    },
+    onUpdate: ({ editor, transaction }) => {
+      setValue(editor.getHTML());
+    },
+  });
+  return (
+    <div>
+      <div className="flex border-b p-4 py-2">
+        {editor && <TipTapMenuBar editor={editor} />}
+      </div>
+      <div ref={ref} className="space-y-2 p-4 pb-0">
+        {expanded && (
+          <>
+            <TagInput
+              suggestions={suggestions?.map((s) => s.address) || []}
+              value={toValues}
+              placeholder="Add tags"
+              label="To"
+              onChange={onToChange}
+            />
+            <TagInput
+              suggestions={suggestions?.map((s) => s.address) || []}
+              value={ccValues}
+              placeholder="Add tags"
+              label="Cc"
+              onChange={onCcChange}
+            />
+            <Input
+              id="subject"
+              className="w-full"
+              placeholder="Subject"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+            />
+          </>
+        )}
+        <div className="flex items-center gap-2">
+          <div
+            className="cursor-pointer"
+            onClick={() => setExpanded((e) => !e)}
+          >
+            <span className="font-medium text-green-600">Draft </span>
+            <span>to {to.join(", ")}</span>
+          </div>
+          AIComposeButton
+        </div>
+      </div>
+      <div className="prose w-full px-4">
+        <EditorContent
+          value={value}
+          editor={editor}
+          placeholder="Write your email here..."
+        />
+      </div>
+      <Separator />
+      <div className="flex items-center justify-between px-4 py-3">
+        <span className="text-sm">
+          Tip: Press{" "}
+          <kbd className="rounded-lg border border-gray-200 bg-gray-100 px-2 py-1.5 text-xs font-semibold text-gray-800">
+            Cmd + J
+          </kbd>{" "}
+          for AI autocomplete
+        </span>
+        <Button
+          onClick={async () => {
+            editor?.commands.clearContent();
+            await handleSend(value);
+          }}
+          //isLoading={isSending}
+        >
+          Send
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default EmailEditor;
