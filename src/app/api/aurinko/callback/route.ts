edddff7acdf6a574f56f59d233a0d022802877ab
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-11-27 14:59:08 +0100

    add arinko lib and callback

---
+++ src/app/api/aurinko/callback/route.ts
@@ -0,0 +1,34 @@
+import { getAccountDetails, getAurinkoToken } from "@/lib/aurinko";
+import { db } from "@/server/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req:NextRequest) => {
+    const { userId } = await auth()
+    if (!userId) return NextResponse.json({ error: "UNAUTHORIZED" }, { status: 401 });
+
+    const params = req.nextUrl.searchParams
+    const status = params.get('status');
+    if (status !== 'success') return NextResponse.json({ error: "Account connection failed" }, { status: 400 });
+
+    const code = params.get('code');
+    const token = await getAurinkoToken(code as string)
+    if (!token) return NextResponse.json({ error: "Failed to fetch token" }, { status: 400 });
+    const accountDetails = await getAccountDetails(token.accessToken)
+    await db.accounts.upsert({
+        where: { id: token.accountId.toString() },
+        create: {
+            id: token.accountId.toString(),
+            userId,
+            token: token.accessToken,
+            provider: 'Aurinko',
+            email: accountDetails.email,
+            name: accountDetails.name
+        },
+        update: {
+            token: token.accessToken,
+        }
+    })
+
+    return NextResponse.redirect(new URL('/mail', req.url))
+}
