7d5430789f4aa75888b528e2618045c33782f9da
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-11-27 20:05:02 +0100

    update mail.ts router add authoriseAccountAccess getNumThreads

--- src/server/api/routers/mail.ts
+++ src/server/api/routers/mail.ts
@@ -1,17 +1,90 @@
-import Account from "@/lib/account";
-import { emailAddressSchema } from "@/lib/types";
 import { Prisma } from "@prisma/client";
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { db } from "@/server/db";
+
+export const authoriseAccountAccess = async (
+  accountId: string,
+  userId: string,
+) => {
+  const account = await db.accounts.findFirst({
+    where: {
+      id: accountId,
+      userId: userId,
+    },
+    select: {
+      id: true,
+      email: true,
+      name: true,
+      token: true,
+    },
+  });
+  if (!account) throw new Error("Invalid token");
+  return account;
+};
+
+const inboxFilter = (accountId: string): Prisma.ThreadWhereInput => ({
+  accountId,
+  inboxStatus: true,
+});
+
+const sentFilter = (accountId: string): Prisma.ThreadWhereInput => ({
+  accountId,
+  sentStatus: true,
+});
+
+const draftFilter = (accountId: string): Prisma.ThreadWhereInput => ({
+  accountId,
+  draftStatus: true,
+});
 
 export const mailRouter = createTRPCRouter({
-    getAccounts: protectedProcedure.query(async ({ ctx }) => {
-        return await ctx.db.accounts.findMany({
-            where: {
-                userId: ctx.auth.userId,
-            }, select: {
-                id: true, email: true, name: true
-            }
-        })
+  getAccounts: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.accounts.findMany({
+      where: {
+        userId: ctx.auth.userId,
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+      },
+    });
+  }),
+  getNumThreads: protectedProcedure
+    .input(
+      z.object({
+        accountId: z.string(),
+        tab: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const account = await authoriseAccountAccess(
+        input.accountId,
+        ctx.auth.userId,
+      );
+      let filter: Prisma.ThreadWhereInput = {};
+      switch (input.tab) {
+        case "inbox":
+          filter = inboxFilter(account.id);
+          break;
+        case "sent":
+          filter = sentFilter(account.id);
+          break;
+        case "drafts":
+          filter = draftFilter(account.id);
+          break;
+      }
+
+      /*if (input.tab === "inbox") {
+        filter = inboxFilter(account.id);
+      } else if (input.tab === "sent") {
+        filter = sentFilter(account.id);
+      } else if (input.tab === "drafts") {
+        filter = draftFilter(account.id);
+      }*/
+      return await ctx.db.thread.count({
+        where: filter,
+      });
     }),
 });
