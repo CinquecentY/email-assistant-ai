19cce387734ab9ac13ae7ece13f2847543800894
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-11-28 23:55:26 +0100

    (fix) move clerk webhook route

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
-  "/api/webhooks(.*)",
+  "/api/clerk/webhook(.*)",
   "/api/initial-sync(.*)",
   "/api/aurinko/webhook(.*)",
   "/privacy",
