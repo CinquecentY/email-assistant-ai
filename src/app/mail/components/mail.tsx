fb6ddf549c43c6cd8debce020e4c2de0f2005f4c
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-11-29 22:13:15 +0100

    update.tsx add Suspense

--- src/app/mail/components/mail.tsx
+++ src/app/mail/components/mail.tsx
@@ -7,7 +7,7 @@ import {
 import { Separator } from "@/components/ui/separator";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
-import React from "react";
+import React, { Suspense } from "react";
 import { AccountSwitch } from "./account-switch";
 import Sidebar from "./sidebar";
 import ThreadList from "./thread-list";
@@ -42,72 +42,74 @@ export function Mail({
     }
   }, []);
   return (
-    <TooltipProvider delayDuration={0}>
-      <ResizablePanelGroup
-        direction="horizontal"
-        onLayout={(sizes: number[]) => {
-          document.cookie = `react-resizable-panels:layout:mail=${JSON.stringify(
-            sizes,
-          )}`;
-        }}
-        className="h-full min-h-screen items-stretch"
-      >
-        <ResizablePanel
-          defaultSize={defaultLayout[0]}
-          collapsedSize={navCollapsedSize}
-          collapsible={true}
-          minSize={15}
-          maxSize={40}
-          onCollapse={() => {
-            setIsCollapsed(true);
-            document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
-              true,
+    <Suspense>
+      <TooltipProvider delayDuration={0}>
+        <ResizablePanelGroup
+          direction="horizontal"
+          onLayout={(sizes: number[]) => {
+            document.cookie = `react-resizable-panels:layout:mail=${JSON.stringify(
+              sizes,
             )}`;
           }}
-          onResize={() => {
-            setIsCollapsed(false);
-            document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
-              false,
-            )}`;
-          }}
-          className={cn(
-            isCollapsed &&
-              "min-w-[50px] transition-all duration-300 ease-in-out",
-          )}
+          className="h-full min-h-screen items-stretch"
         >
-          <div className="flex h-full flex-1 flex-col">
-            <div
-              className={cn(
-                "flex h-11 items-center justify-center",
-                isCollapsed ? "h-11" : "px-2",
-              )}
-            >
-              <AccountSwitch isCollapsed={isCollapsed} />
+          <ResizablePanel
+            defaultSize={defaultLayout[0]}
+            collapsedSize={navCollapsedSize}
+            collapsible={true}
+            minSize={15}
+            maxSize={40}
+            onCollapse={() => {
+              setIsCollapsed(true);
+              document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
+                true,
+              )}`;
+            }}
+            onResize={() => {
+              setIsCollapsed(false);
+              document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
+                false,
+              )}`;
+            }}
+            className={cn(
+              isCollapsed &&
+                "min-w-[50px] transition-all duration-300 ease-in-out",
+            )}
+          >
+            <div className="flex h-full flex-1 flex-col">
+              <div
+                className={cn(
+                  "flex h-11 items-center justify-center",
+                  isCollapsed ? "h-11" : "px-2",
+                )}
+              >
+                <AccountSwitch isCollapsed={isCollapsed} />
+              </div>
+              <Separator />
+              <Sidebar isCollapsed={isCollapsed} />
+              <div className="flex-1"></div>
+              <AskAI isCollapsed={isCollapsed} />
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
+            <div className="flex items-center gap-4 px-4 py-2">
+              <Button className={"h-7 md:hidden"} variant={"secondary"}>
+                <ChevronLeft />
+              </Button>
+              <h1 className="text-xl font-bold">Inbox</h1>
             </div>
             <Separator />
-            <Sidebar isCollapsed={isCollapsed} />
-            <div className="flex-1"></div>
-            <AskAI isCollapsed={isCollapsed} />
-          </div>
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={defaultLayout[1]} minSize={30}>
-          <div className="flex items-center gap-4 px-4 py-2">
-            <Button className={"h-7 md:hidden"} variant={"secondary"}>
-              <ChevronLeft />
-            </Button>
-            <h1 className="text-xl font-bold">Inbox</h1>
-          </div>
-          <Separator />
-          <SearchBar />
-          <ThreadList />
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={defaultLayout[2]} minSize={30}>
-          <ThreadDisplay />
-        </ResizablePanel>
-      </ResizablePanelGroup>
-    </TooltipProvider>
+            <SearchBar />
+            <ThreadList />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={defaultLayout[2]} minSize={30}>
+            <ThreadDisplay />
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </TooltipProvider>
+    </Suspense>
   );
 }
 
