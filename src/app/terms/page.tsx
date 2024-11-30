bae36bc0e4d5781c7b78847b82fe1b72c870e95a
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-11-30 11:22:51 +0100

    add privary and terms page

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,126 @@
+import React from "react";
+
+function Terms() {
+  return (
+    <div className="mx-auto max-w-7xl py-12 md:px-8">
+      <>
+        <div className="mx-auto max-w-5xl px-4 md:px-8">
+          <h1 className="mb-2 text-center text-3xl font-semibold text-gray-700">
+            Terms and Conditions
+          </h1>
+          <h2 className="mb-20 text-center lowercase text-gray-600">
+            Last updated December 30th, 2024
+          </h2>
+          <h3 className="mb-5 text-left text-2xl font-semibold text-gray-600">
+            1. Introduction
+          </h3>
+          <p className="mb-10 mt-5 text-justify leading-relaxed text-gray-600">
+            By using Y.Ah, Inc you confirm your acceptance of, and agree to be
+            bound by, these terms and conditions.
+          </p>
+          <h3 className="mb-5 text-left text-2xl font-semibold text-gray-600">
+            2. Agreement to Terms and Conditions
+          </h3>
+          <p className="mb-10 mt-5 text-justify leading-relaxed text-gray-600">
+            This Agreement takes effect on the date on which you first use the
+            Y.Ah, Inc application.
+          </p>
+          <h3 className="mb-5 text-left text-2xl font-semibold text-gray-600">
+            3. Unlimited Access Software License with Termination Rights
+          </h3>
+          <p className="mb-10 mt-5 text-justify leading-relaxed text-gray-600">
+            The Y.Ah, Inc Software License facilitates the acquisition of Y.Ah,
+            Inc software through a single purchase, granting users unrestricted
+            and perpetual access to its comprehensive functionalities. Tailored
+            for independent creators, entrepreneurs, and small businesses, Y.Ah,
+            Inc empowers users to create compelling web pages and online
+            portfolios. This license entails a straightforward and flexible
+            arrangement, exempting users from recurring fees or subscriptions.
+            However, it is important to acknowledge that the licensor retains
+            the right to terminate the license without conditions or
+            prerequisites. This termination provision enables the licensor to
+            exercise control over software distribution and utilization. Opting
+            for the Y.Ah, Inc Software License enables users to enjoy the
+            benefits of the software while recognizing the licensor's
+            unrestricted termination rights, which provide adaptability and
+            address potential unforeseen circumstances.
+          </p>
+          <h3 className="mb-5 text-left text-2xl font-semibold text-gray-600">
+            4. Refunds
+          </h3>
+          <p className="mb-10 mt-5 text-justify leading-relaxed text-gray-600">
+            Due to the nature of digital products, the Y.Ah, Inc boilerplate
+            cannot be refunded or exchanged once access is granted.
+          </p>
+          <h3 className="mb-5 text-left text-2xl font-semibold text-gray-600">
+            5. Disclaimer
+          </h3>
+          <p className="mb-10 mt-5 text-justify leading-relaxed text-gray-600">
+            It is not warranted that Y.Ah, Inc will meet your requirements or
+            that its operation will be uninterrupted or error free. All express
+            and implied warranties or conditions not stated in this Agreement
+            (including without limitation, loss of profits, loss or corruption
+            of data, business interruption or loss of contracts), so far as such
+            exclusion or disclaimer is permitted under the applicable law are
+            excluded and expressly disclaimed. This Agreement does not affect
+            your statutory rights.
+          </p>
+          <h3 className="mb-5 text-left text-2xl font-semibold text-gray-600">
+            6. Warranties and Limitation of Liability
+          </h3>
+          <p className="mb-10 mt-5 text-justify leading-relaxed text-gray-600">
+            Y.Ah, Inc does not give any warranty, guarantee or other term as to
+            the quality, fitness for purpose or otherwise of the software. Y.Ah,
+            Inc shall not be liable to you by reason of any representation
+            (unless fraudulent), or any implied warranty, condition or other
+            term, or any duty at common law, for any loss of profit or any
+            indirect, special or consequential loss, damage, costs, expenses or
+            other claims (whether caused by Y.Ah, Inc's negligence or the
+            negligence of its servants or agents or otherwise) which arise out
+            of or in connection with the provision of any goods or services by
+            Y.Ah, Inc. Y.Ah, Inc shall not be liable or deemed to be in breach
+            of contract by reason of any delay in performing, or failure to
+            perform, any of its obligations if the delay or failure was due to
+            any cause beyond its reasonable control. Notwithstanding contrary
+            clauses in this Agreement, in the event that Y.Ah, Inc are deemed
+            liable to you for breach of this Agreement, you agree that Y.Ah,
+            Inc's liability is limited to the amount actually paid by you for
+            your services or software, which amount calculated in reliance upon
+            this clause. You hereby release Y.Ah, Inc from any and all
+            obligations, liabilities and claims in excess of this limitation.
+          </p>
+          <h3 className="mb-5 text-left text-2xl font-semibold text-gray-600">
+            7. Responsibilities
+          </h3>
+          <p className="mb-10 mt-5 text-justify leading-relaxed text-gray-600">
+            Y.Ah, Inc is not responsible for what the user does with the
+            user-generated content.
+          </p>
+          <h3 className="mb-5 text-left text-2xl font-semibold text-gray-600">
+            8. Price Adjustments
+          </h3>
+          <p className="mb-10 mt-5 text-justify leading-relaxed text-gray-600">
+            As we continue to improve Y.Ah, Inc and expand our offerings, the
+            price may increase. The discount is provided to help customers
+            secure the current price without being surprised by future
+            increases.
+          </p>
+          <h3 className="mb-5 text-left text-2xl font-semibold text-gray-600">
+            9. General Terms and Law
+          </h3>
+          <p className="mb-10 mt-5 text-justify leading-relaxed text-gray-600">
+            This Agreement is governed by the laws of Singapore. You acknowledge
+            that no joint venture, partnership, employment, or agency
+            relationship exists between you and Y.Ah, Inc as a result of your
+            use of these services. You agree not to hold yourself out as a
+            representative, agent or employee of Y.Ah, Inc. You agree that Y.Ah,
+            Inc will not be liable by reason of any representation, act or
+            omission to act by you.
+          </p>
+        </div>
+      </>
+    </div>
+  );
+}
+
+export default Terms;
