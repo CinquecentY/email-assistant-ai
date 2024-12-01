0034c34606d14546106b6113d4136cd67d89f63c
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-12-01 20:00:51 +0100

    update hover-bar.tsx update styling

--- src/components/hover-bar.tsx
+++ src/components/hover-bar.tsx
@@ -7,33 +7,33 @@ import { useAuth, UserButton } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { House } from "lucide-react";
+import { motion } from "framer-motion";
 
 const HoverBar = () => {
   const { isSignedIn } = useAuth();
   const pathname = usePathname();
   return (
-    <div
-      className={`absolute bottom-10 left-2 z-10 md:left-3 ${pathname === "/mail" && "bottom-1"}`}
+    <motion.div
+      layoutId="hover-bar"
+      className="fixed bottom-10 left-2 z-10 flex flex-col items-center gap-3 rounded-xl bg-background p-2 shadow-md md:left-3 md:flex-row md:px-4"
     >
-      <div className="flex flex-col items-center gap-3 rounded-xl bg-background p-2 md:flex-row md:px-4">
-        {isSignedIn && (
-          <Button variant={"ghost"} className="rounded-full p-1">
-            <UserButton />
-          </Button>
-        )}
-        <Button variant={"ghost"} className="rounded-full p-2" asChild>
-          <Link href="/">
-            <House className="h-[1.2rem] w-[1.2rem]" />
-          </Link>
+      {isSignedIn && pathname === "/mail" && (
+        <span className="md:w-56">
+          <ComposeButton />
+        </span>
+      )}
+      {isSignedIn && (
+        <Button variant={"ghost"} className="rounded-full p-1">
+          <UserButton />
         </Button>
-        <ModeToggle />
-        {isSignedIn && pathname === "/mail" && (
-          <span className="md:w-56">
-            <ComposeButton />
-          </span>
-        )}
-      </div>
-    </div>
+      )}
+      <Button variant={"ghost"} className="rounded-full p-2" asChild>
+        <Link href="/">
+          <House className="h-[1.2rem] w-[1.2rem]" />
+        </Link>
+      </Button>
+      <ModeToggle />
+    </motion.div>
   );
 };
 
