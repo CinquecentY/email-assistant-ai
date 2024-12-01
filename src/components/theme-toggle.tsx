83ff55fd779bb056244f72b40faa3127f1fe7b04
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-12-01 14:01:22 +0100

    update theme-toggle.tsx add system option

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,27 +1,53 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
 
 export function ModeToggle() {
-    const { theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme();
 
-    const toggleTheme = () => {
-        setTheme(theme === "light" ? "dark" : "light")
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  const getTheme = () => {
+    switch (theme) {
+      case "light":
+        return "sunny";
+      case "dark":
+        return "nocturnal";
+      default:
+        return "your choice";
     }
+  };
 
-    return (
-        <Button
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
             variant="outline"
             size="icon"
             onClick={toggleTheme}
             aria-label="Toggle theme"
-        >
+          >
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        </Button>
-    )
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{getTheme()}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
 }
