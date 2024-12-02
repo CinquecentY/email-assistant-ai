205087a024ef6be9a05a2d31fb39a369da0cd74f
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-12-03 00:39:45 +0100

    update templates-mobile.tsx

--- src/app/templates/templates-dashboard/components/templates-mobile.tsx
+++ src/app/templates/templates-dashboard/components/templates-mobile.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/dialog";
 import { ResizablePanel } from "@/components/ui/resizable";
 import { Separator } from "@/components/ui/separator";
-import { templatesTabAtom } from "@/lib/atoms";
+import { templatesAtom } from "@/lib/atoms";
 import { cn } from "@/lib/utils";
 import { Tabs, TabsContent } from "@radix-ui/react-tabs";
 import { formatDistance, subDays } from "date-fns";
@@ -19,25 +19,13 @@ import { useAtom } from "jotai";
 import { ArrowLeft, Trash2 } from "lucide-react";
 import React from "react";
 import TemplateEditor from "./template-editor";
+import { type Template } from "@/lib/types";
 
 const TemplatesMobile = () => {
   const [tab, setTab] = React.useState("templates");
   const [templateId, setTemplateId] = React.useState(0);
 
-  const [templates, setTemplates] = React.useState(
-    [...Array(20)].map((_, i) => ({
-      id: i + 1,
-      name: "Template " + i,
-      date: formatDistance(subDays(new Date(), 3), new Date(), {
-        addSuffix: true,
-      }),
-      text: `<p>Dear man,</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    `,
-    })),
-  );
+  const [templates, setTemplates] = useAtom<Template[]>(templatesAtom);
 
   function deleteTemplate(id: number) {
     setTemplates(templates.filter((template) => template.id !== id));
@@ -52,66 +40,78 @@ const TemplatesMobile = () => {
           </div>
           <Separator />
           <article className="flex h-full max-h-[calc(100vh-50px)] flex-1 flex-col gap-2 overflow-y-auto bg-background p-4 pt-2">
-            {templates.map((template) => (
-              <div
-                key={template.id}
-                className={cn(
-                  "relative flex h-auto flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm text-muted-foreground transition-all hover:text-accent-foreground",
-                )}
-              >
-                <span className="inline-flex w-full gap-2">
-                  <span
-                    className="flex flex-1 flex-col"
-                    onClick={() => {
-                      setTemplateId(template.id);
-                      setTab("template");
-                    }}
-                  >
-                    <div className="w-fit cursor-pointer font-bold">
-                      {template.name}
-                    </div>
-                    <div
-                      className={cn(
-                        "text-xs font-medium",
-                        templateId
-                          ? "text-foreground"
-                          : "text-muted-foreground",
-                      )}
+            {templates.length === 0 ? (
+              <>
+                <div className="flex h-full items-center justify-center">
+                  <div className="text-muted-foreground">
+                    No templates found
+                  </div>
+                </div>
+              </>
+            ) : (
+              templates.map((template) => (
+                <div
+                  key={template.id}
+                  className={cn(
+                    "relative flex h-auto flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm text-muted-foreground transition-all hover:text-accent-foreground",
+                  )}
+                >
+                  <span className="inline-flex w-full gap-2">
+                    <span
+                      className="flex flex-1 flex-col"
+                      onClick={() => {
+                        setTemplateId(template.id!);
+                        setTab("template");
+                      }}
                     >
-                      {template.date}
-                    </div>
+                      <div className="w-fit cursor-pointer font-bold">
+                        {template.name}
+                      </div>
+                      <div
+                        className={cn(
+                          "text-xs font-medium",
+                          templateId
+                            ? "text-foreground"
+                            : "text-muted-foreground",
+                        )}
+                      >
+                        {formatDistance(subDays(template.date, 3), new Date(), {
+                          addSuffix: true,
+                        })}
+                      </div>
+                    </span>
+                    <span>
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button className="rounded-full bg-transparent text-foreground hover:bg-destructive hover:text-destructive-foreground">
+                            <Trash2 />
+                          </Button>
+                        </DialogTrigger>
+                        <DialogContent className="rounded-xl">
+                          <DialogHeader>
+                            <DialogTitle>Delete Template?</DialogTitle>
+                            <DialogDescription>
+                              This action is irreversible.
+                            </DialogDescription>
+                          </DialogHeader>
+                          <DialogFooter>
+                            <DialogClose asChild>
+                              <Button
+                                variant={"destructive"}
+                                type="submit"
+                                onClick={() => deleteTemplate(template.id!)}
+                              >
+                                Confirm
+                              </Button>
+                            </DialogClose>
+                          </DialogFooter>
+                        </DialogContent>
+                      </Dialog>
+                    </span>
                   </span>
-                  <span>
-                    <Dialog>
-                      <DialogTrigger asChild>
-                        <Button className="rounded-full bg-transparent text-foreground hover:bg-destructive hover:text-destructive-foreground">
-                          <Trash2 />
-                        </Button>
-                      </DialogTrigger>
-                      <DialogContent className="rounded-xl">
-                        <DialogHeader>
-                          <DialogTitle>Delete Template?</DialogTitle>
-                          <DialogDescription>
-                            This action is irreversible.
-                          </DialogDescription>
-                        </DialogHeader>
-                        <DialogFooter>
-                          <DialogClose asChild>
-                            <Button
-                              variant={"destructive"}
-                              type="submit"
-                              onClick={() => deleteTemplate(template.id)}
-                            >
-                              Confirm
-                            </Button>
-                          </DialogClose>
-                        </DialogFooter>
-                      </DialogContent>
-                    </Dialog>
-                  </span>
-                </span>
-              </div>
-            ))}
+                </div>
+              ))
+            )}
           </article>
         </TabsContent>
         <TabsContent value="template" className="h-screen w-full">
