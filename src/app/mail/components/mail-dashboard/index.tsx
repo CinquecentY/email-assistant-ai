11a3ab6aac881ca0a34c6511e8484f52a1b5fe6d
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-12-03 00:35:40 +0100

    update mail-dashboard

--- src/app/mail/components/mail-dashboard/index.tsx
+++ src/app/mail/components/mail-dashboard/index.tsx
@@ -1,15 +1,29 @@
 "use client";
-import DashboardLayout from "@/components/layout/dashboard-layout";
 import React from "react";
-import MailDisplay from "./components/mail-display";
 import { useSearchParams, redirect } from "next/navigation";
 import { useLocalStorage } from "usehooks-ts";
 import { useIsMobile } from "@/hooks/use-mobile";
+import DashboardLayout from "@/components/layout/dashboard-layout";
+import MailDisplay from "./components/mail-display";
 import MailMobile from "./components/mail-mobile";
+/*import dynamic from "next/dynamic";
 
+const MailMobile = dynamic(() => import("./components/mail-mobile"), {
+  ssr: false,
+});
+const MailDisplay = dynamic(() => import("./components/mail-display"), {
+  ssr: false,
+});
+const DashboardLayout = dynamic(
+  () => import("@/components/layout/dashboard-layout"),
+  {
+    ssr: false,
+  },
+);*/
 const MailDashboard = () => {
   const searchParams = useSearchParams();
   const [accountId, setAccountId] = useLocalStorage("accountId", "");
+  const isMobile = useIsMobile();
 
   React.useEffect(() => {
     const accountIdParams = searchParams.get("accountId");
@@ -18,7 +32,6 @@ const MailDashboard = () => {
       redirect("/mail");
     }
   }, [accountId, searchParams, setAccountId]);
-  const isMobile = useIsMobile();
   return (
     <DashboardLayout>
       {isMobile ? <MailMobile /> : <MailDisplay />}
