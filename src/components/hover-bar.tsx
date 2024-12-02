059f2ee177cf5c76fe6f7f3f798ded2086c4cc8a
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-12-02 01:31:41 +0100

    update hover-bar.tsx add ask ai

--- src/components/hover-bar.tsx
+++ src/components/hover-bar.tsx
@@ -6,15 +6,27 @@ import ComposeButton from "@/app/mail/components/compose-button";
 import { useAuth, UserButton } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { House, Loader2 } from "lucide-react";
+import { House, Loader2, Sparkle, Sparkles } from "lucide-react";
 import useThreads from "@/app/mail/use-threads";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { useIsMobile } from "@/hooks/use-mobile";
+import AskAI from "@/app/mail/components/ask-ai";
 
 const HoverBar = () => {
   const { isSignedIn } = useAuth();
   const pathname = usePathname();
   const { isFetching } = useThreads();
+  const isMobile = useIsMobile();
+  // TODO arial-label all buttons
   return (
-    <div className="fixed bottom-1 left-2 z-10 flex flex-col items-center gap-3 rounded-xl bg-background p-2 shadow-md md:left-0 md:flex-row md:px-4">
+    <div className="fixed bottom-1 left-1 z-10 flex flex-col items-center gap-3 rounded-xl bg-background p-2 shadow-md md:left-0 md:flex-row md:px-4">
       {isSignedIn && pathname === "/mail" && isFetching && (
         <Button
           variant={"ghost"}
@@ -22,7 +34,7 @@ const HoverBar = () => {
           asChild
         >
           <span>
-            <Loader2 className="size-fit h-4 w-4 animate-spin" />
+            <Loader2 className="h-[1.2rem] w-[1.2rem] animate-spin" />
           </span>
         </Button>
       )}
@@ -37,6 +49,21 @@ const HoverBar = () => {
         </Link>
       </Button>
       <ModeToggle />
+      {isSignedIn && isMobile && (
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant={"ghost"} className="rounded-full p-1">
+              <Sparkles className="h-[1.2rem] w-[1.2rem]" />
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Ask AI</DialogTitle>
+            </DialogHeader>
+            <AskAI isCollapsed={false} />
+          </DialogContent>
+        </Dialog>
+      )}
       {isSignedIn && pathname === "/mail" && (
         <>
           <span className="md:w-56">
@@ -49,7 +76,7 @@ const HoverBar = () => {
               asChild
             >
               <span>
-                <Loader2 className="size-fit h-4 w-4 animate-spin" />
+                <Loader2 className="h-[1.2rem] w-[1.2rem] animate-spin" />
               </span>
             </Button>
           )}
