aefed4e0c3e7de0a6153288d3f5579b5aa5a49d8
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-12-02 01:55:27 +0100

    update email assistant name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Mail Assistant ΛI",
+  title: "Mail Assistant ΛＩ",
   description: "Concept by Yassine Annagrebah",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
--- src/components/layout/home-layout.tsx
+++ src/components/layout/home-layout.tsx
@@ -11,7 +11,7 @@ const HomeLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
             href="/"
             className="border-b-2 px-2 font-semibold lowercase md:text-2xl"
           >
-            Mail Assistant ΛI
+            Mail Assistant ΛＩ
           </Link>
         </span>
       </nav>
