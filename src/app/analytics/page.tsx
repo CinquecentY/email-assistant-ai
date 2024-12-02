1ac63e76e287a967658c2ca9e162c4867209030b
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-12-02 17:15:03 +0100

    add /analytics page

---
+++ src/app/analytics/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AnalyticsDashboard from "./components/analytics-dashboard";
+
+const Analytics = () => {
+  return <AnalyticsDashboard />;
+};
+
+export default Analytics;
