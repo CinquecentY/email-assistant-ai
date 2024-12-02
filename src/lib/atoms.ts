e5eeb9ca13b6babb4d5187ba91178dca85a8b93f
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-12-03 00:27:09 +0100

    add templateAtom

--- src/lib/atoms.ts
+++ src/lib/atoms.ts
@@ -1,4 +1,5 @@
 import { atom } from "jotai";
+import { Template } from "./types";
 
 export const configAtom = atom<string | null>(null);
 export const isSearchingAtom = atom(false);
@@ -6,3 +7,4 @@ export const searchValueAtom = atom("");
 export const tabAtom = atom("inbox");
 export const isCollapsedAtom = atom(false);
 export const templatesTabAtom = atom("templates");
+export const templatesAtom = atom<Template[]>([])
