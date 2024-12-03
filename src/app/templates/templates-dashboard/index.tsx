680939ee94d711e90929fb8d3fe773c6ee088cde
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-12-03 13:37:19 +0100

    add templates-dashboard component

--- src/app/templates/templates-dashboard/index.tsx
+++ src/app/templates/templates-dashboard/index.tsx
@@ -1,14 +1,30 @@
-"use client"
-import DashboardLayout from "@/components/layout/dashboard-layout";
+"use client";
 import React from "react";
 import TemplatesMobile from "./components/templates-mobile";
+import { useIsMobile } from "@/hooks/use-mobile";
+import dynamic from "next/dynamic";
+import { Loader2 } from "lucide-react";
+import TemplatesDisplay from "./components/templates-display";
+
+const DashboardLayout = dynamic(
+  () => import("@/components/layout/dashboard-layout"),
+  {
+    loading: () => (
+      <section className="flex min-h-screen items-center justify-center">
+        <Loader2 className="size-32 animate-spin" />
+      </section>
+    ),
+    ssr: false,
+  },
+);
 
 const TemplatesDashboard = () => {
+  const isMobile = useIsMobile();
   return (
     <DashboardLayout>
-      <TemplatesMobile/>
+      {isMobile ? <TemplatesMobile /> : <TemplatesDisplay />}
     </DashboardLayout>
   );
 };
-// Swap useLocalStorage with Atom
+// TODO Swap useLocalStorage with Cookies
 export default TemplatesDashboard;
