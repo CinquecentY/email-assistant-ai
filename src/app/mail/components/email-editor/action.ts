8aeb146aa50b4f680870590cb7c321bf082f0c9d
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-12-04 22:40:50 +0100

    update action.ts add getDataKnowledge

--- src/app/mail/components/email-editor/action.ts
+++ src/app/mail/components/email-editor/action.ts
@@ -8,6 +8,7 @@ import {
   polishTextPrompt,
   replyEmailPrompt,
 } from "@/lib/ai/prompts/prompts";
+import { api } from "@/trpc/server";
 
 export async function replyToEmail(context: string, prompt: string) {
   const result = streamText({
@@ -28,10 +29,17 @@ export async function composeEmail(
   const { hits } = await oramaManager.vectorSearch({
     prompt,
   });
+  const dataKnowledge = await getDataKnowledge();
   const result = streamText({
     model: googleModel,
     ...composePrompt(
-      hits.map((hit) => JSON.stringify(hit.document)).join("\n"),
+      [
+        "This is the data related to the user's emails",
+        hits.map((hit) => JSON.stringify(hit.document)).join("\n"),
+        "-----------",
+        dataKnowledge,
+        "-----------",
+      ].join("\n"),
       context,
       prompt,
     ),
@@ -56,3 +64,35 @@ export async function polishText(prompt: string) {
 
   return result.textStream;
 }
+
+const getDataKnowledge = async () => {
+  const clients = await api.data.getClients();
+  const documents = await api.data.getDocuments();
+  const events = await api.data.getEvents();
+  const history = await api.data.getHistory();
+  const notes = await api.data.getNotes();
+
+  const clientData = clients.map((client, i) => {
+    return `Client ${i}:\n -name: ${client.name}\n - email: ${client.email}\n  - phoneNumber: ${client.phoneNumber}\n  - city: ${client.city}\n  - country: ${client.country}\n  - status: ${client.status}\n `;
+  });
+  const documentData = documents.map((document, i) => {
+    return `Document ${i}:\n - type: ${document.type}\n  - status: ${document.status}\n  - amount: ${document.amount}\n  - products: ${document.products}\n `;
+  });
+  const eventData = events.map((event, i) => {
+    return `Event ${i}:\n  - name: ${event.name}\n - description: ${event.description}\n- startingAt: ${event.startingAt.toLocaleDateString()}\n `;
+  });
+  const historyData = history.map((history, i) => {
+    return `History ${i}:\n clientId: ${history.clientId}\n  - action: ${history.action}\n - created At: ${history.createdAt.toLocaleDateString()}\n `;
+  });
+  const notesData = notes.map((note, i) => {
+    return `Note ${i};\n - clientId: ${note.clientId}\n  - description: ${note.description}\n  - createdAt: ${note.createdAt.toLocaleDateString()}\n `;
+  });
+  return [
+    "This is the data related to the definitions provided",
+    clientData,
+    documentData,
+    eventData,
+    historyData,
+    notesData,
+  ].join("\n");
+};
