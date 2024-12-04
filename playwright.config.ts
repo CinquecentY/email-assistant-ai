8e62780576ac717ad06c4e3453bf4c4a66bf4b5d
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-12-04 17:04:20 +0100

    update playwright.config

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,8 @@ import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.resolve(".env") });
 
+const baseURL = process.env.NEXT_PUBLIC_URL ?? "http://127.0.0.1:3000";
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -27,7 +29,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-    baseURL: process.env.NEXT_PUBLIC_URL ?? "http://127.0.0.1:3000",
+    baseURL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
@@ -99,9 +101,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: "npm run start",
-  //   url: "http://127.0.0.1:3000",
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run start",
+    url: baseURL,
+    reuseExistingServer: !process.env.CI,
+  },
 });
