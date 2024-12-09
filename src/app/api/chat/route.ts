3e56ceef663345d50959502c3da29ee24c760dd7
cinqye <81559422+CinquecentY@users.noreply.github.com>
2024-12-09 17:57:05 +0100

    update getDataKnowledge

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,3 +1,4 @@
+import { getDataKnowledge } from "@/lib/ai/action";
 import { googleModel } from "@/lib/ai/models/google";
 import { chatPrompt } from "@/lib/ai/prompts/prompts";
 import { OramaManager } from "@/lib/orama";
@@ -58,35 +59,3 @@ export async function POST(req: Request) {
     );
   }
 }
-
-const getDataKnowledge = async () => {
-  const clients = await api.data.getClients();
-  const documents = await api.data.getDocuments();
-  const events = await api.data.getEvents();
-  const history = await api.data.getHistory();
-  const notes = await api.data.getNotes();
-
-  const clientData = clients.map((client, i) => {
-    return `Client ${i}:\n -name: ${client.name}\n - email: ${client.email}\n  - phoneNumber: ${client.phoneNumber}\n  - city: ${client.city}\n  - country: ${client.country}\n  - status: ${client.status}\n `;
-  });
-  const documentData = documents.map((document, i) => {
-    return `Document ${i}:\n - type: ${document.type}\n  - status: ${document.status}\n  - amount: ${document.amount}\n  - products: ${document.products}\n `;
-  });
-  const eventData = events.map((event, i) => {
-    return `Event ${i}:\n  - name: ${event.name}\n - description: ${event.description}\n- startingAt: ${event.startingAt.toLocaleDateString()}\n `;
-  });
-  const historyData = history.map((history, i) => {
-    return `History ${i}:\n clientId: ${history.clientId}\n  - action: ${history.action}\n - created At: ${history.createdAt.toLocaleDateString()}\n `;
-  });
-  const notesData = notes.map((note, i) => {
-    return `Note ${i};\n - clientId: ${note.clientId}\n  - description: ${note.description}\n  - createdAt: ${note.createdAt.toLocaleDateString()}\n `;
-  });
-  return [
-    "This is the data related to the definitions provided",
-    clientData,
-    documentData,
-    eventData,
-    historyData,
-    notesData,
-  ].join("\n");
-};
--- src/lib/ai/action.ts
+++ src/lib/ai/action.ts
@@ -65,7 +65,7 @@ export async function polishText(prompt: string) {
   return result.textStream;
 }
 
-const getDataKnowledge = async () => {
+export const getDataKnowledge = async () => {
   const clients = await api.data.getClients();
   const documents = await api.data.getDocuments();
   const events = await api.data.getEvents();
@@ -76,19 +76,19 @@ const getDataKnowledge = async () => {
     return `Client ${i}:\n -name: ${client.name}\n - email: ${client.email}\n  - phoneNumber: ${client.phoneNumber}\n  - city: ${client.city}\n  - country: ${client.country}\n  - status: ${client.status}\n `;
   });
   const documentData = documents.map((document, i) => {
-    return `Document ${i}:\n - type: ${document.type}\n  - status: ${document.status}\n  - amount: ${document.amount}\n  - products: ${document.products}\n `;
+    return `Document ${i}:\n  - clientName: ${document.Client.name}\n - type: ${document.type}\n  - status: ${document.status}\n  - amount: ${document.amount}\n  - products: ${document.products}\n  - createdAt:${document.createdAt.toLocaleDateString()} `;
   });
   const eventData = events.map((event, i) => {
-    return `Event ${i}:\n  - name: ${event.name}\n - description: ${event.description}\n- startingAt: ${event.startingAt.toLocaleDateString()}\n `;
+    return `Event ${i}:\n - clientName: ${event.Client.name}\n  - name: ${event.name}\n - description: ${event.description}\n- startingAt: ${event.startingAt.toLocaleDateString()}\n - endingAt: ${event.endingAt.toLocaleDateString()}\n `;
   });
   const historyData = history.map((history, i) => {
-    return `History ${i}:\n clientId: ${history.clientId}\n  - action: ${history.action}\n - created At: ${history.createdAt.toLocaleDateString()}\n `;
+    return `History ${i}:\n clientName: ${history.Client.name}\n  - action: ${history.action}\n - createdAt: ${history.createdAt.toLocaleDateString()}\n `;
   });
   const notesData = notes.map((note, i) => {
-    return `Note ${i};\n - clientId: ${note.clientId}\n  - description: ${note.description}\n  - createdAt: ${note.createdAt.toLocaleDateString()}\n `;
+    return `Note ${i};\n - clientName: ${note.Client.name}\n  - description: ${note.description}\n  - createdAt: ${note.createdAt.toLocaleDateString()}\n `;
   });
   return [
-    "This is the data related to the definitions provided",
+    "This is the data related to the definitions provided:",
     clientData,
     documentData,
     eventData,
